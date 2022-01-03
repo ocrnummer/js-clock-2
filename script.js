c47e3d159c5bfb40d9ff77ec2a575498bf8aa76e
Alex <alexanderbergquist80@gmail.com>
2022-01-03 17:25:23 +0100

    Drop down med alternativ för att välja tidszon

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let clock = () => {
-
     let date = new Date();
     let hour = date.getHours();
     let minute = date.getMinutes();
@@ -12,8 +11,41 @@ let clock = () => {
     let time = `${hour}:${minute}:${second}`;
 
     document.querySelector("#clock").innerText = time;
-
     setInterval( () => clock(), 1000);
 }
 
-clock()
+let timezoneOptions = () => {
+
+    document.querySelector("#timezoneEl").innerHTML = ``;
+
+    document.querySelector("#timezoneEl").innerHTML = `
+    <option value="" disabled selected>Välj tidszon</option>
+    <option value="+1">+1</option>
+    <option value="+2">+2</option>
+    <option value="+3">+3</option>
+    <option value="+4">+4</option>
+    <option value="+5">+5</option>
+    <option value="+6">+6</option>
+    <option value="+7">+7</option>
+    <option value="+8">+8</option>
+    <option value="+9">+9</option>
+    <option value="+10">+10</option>
+    <option value="+11">+11</option>
+    <option value="+12">+12</option>
+    <option value="UTC">UTC</option>
+    <option value="-1">-1</option>
+    <option value="-2">-2</option>
+    <option value="-3">-3</option>
+    <option value="-4">-4</option>
+    <option value="-5">-5</option>
+    <option value="-6">-6</option>
+    <option value="-7">-7</option>
+    <option value="-8">-8</option>
+    <option value="-9">-9</option>
+    <option value="-1">-10</option>
+    <option value="-11">-11</option>
+    `
+}
+
+timezoneOptions();
+clock();
