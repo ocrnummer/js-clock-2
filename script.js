254fc8856ac9726f18d16d237b3356857b979dec
Alex <alexanderbergquist80@gmail.com>
2022-01-03 18:50:20 +0100

    Clock shows timezone, but no click event added yet.

--- script.js
+++ script.js
@@ -1,12 +1,69 @@
+const timezones = [
+    "+1",
+    "+2",
+    "+3",
+    "+4",
+    "+5",
+    "+6",
+    "+7",
+    "+8",
+    "+9",
+    "+10",
+    "+11",
+    "+12",
+    "UTC",
+    "-1",
+    "-2",
+    "-3",
+    "-4",
+    "-5",
+    "-6",
+    "-7",
+    "-8",
+    "-9",
+    "-1",
+    "-11"
+]
+
+const timezoneEl = document.querySelector("#timezoneEl");
+
+let timezoneOptions = (zone) => {
+    // Rensa dropdown
+    timezoneEl.innerHTML = ``;
+    
+    // Generera platshållare
+    timezoneEl.innerHTML += `
+        <option id="timezoneOption" value="0" disabled selected>Välj tidszon</option>
+    `;
+
+    // Generera alternativ
+    timezones.forEach(zone => {
+        if (zone === "UTC") {
+            timezoneEl.innerHTML += `
+                <option id="timezoneOption" value="0">${zone}</option>
+        `
+        } else {
+            timezoneEl.innerHTML += `
+                <option id="timezoneOption" value=${zone}>${zone}</option>
+            `
+        };
+    })
+}
+
+
+timezoneOptions();
+
 let clock = () => {
     let date = new Date();
     let hour = date.getHours();
     let minute = date.getMinutes();
     let second = date.getSeconds();
 
-    hour = hour < 10 ?  "0" + hour: hour;
-    minute = minute < 10 ?  "0" + minute: minute;
-    second = second < 10 ?  "0" + second: second;
+    hour = hour < 10 ?  "0" + hour : hour;
+    minute = minute < 10 ?  "0" + minute : minute;
+    second = second < 10 ?  "0" + second : second;
+
+    hour = hour + optionNmb;
 
     let time = `${hour}:${minute}:${second}`;
 
@@ -14,38 +71,7 @@ let clock = () => {
     setInterval( () => clock(), 1000);
 }
 
-let timezoneOptions = () => {
+const optionStr = document.querySelector("#timezoneOption").getAttribute('value');
+const optionNmb = parseInt(optionStr);
 
-    document.querySelector("#timezoneEl").innerHTML = ``;
-
-    document.querySelector("#timezoneEl").innerHTML = `
-    <option value="" disabled selected>Välj tidszon</option>
-    <option value="+1">+1</option>
-    <option value="+2">+2</option>
-    <option value="+3">+3</option>
-    <option value="+4">+4</option>
-    <option value="+5">+5</option>
-    <option value="+6">+6</option>
-    <option value="+7">+7</option>
-    <option value="+8">+8</option>
-    <option value="+9">+9</option>
-    <option value="+10">+10</option>
-    <option value="+11">+11</option>
-    <option value="+12">+12</option>
-    <option value="UTC">UTC</option>
-    <option value="-1">-1</option>
-    <option value="-2">-2</option>
-    <option value="-3">-3</option>
-    <option value="-4">-4</option>
-    <option value="-5">-5</option>
-    <option value="-6">-6</option>
-    <option value="-7">-7</option>
-    <option value="-8">-8</option>
-    <option value="-9">-9</option>
-    <option value="-1">-10</option>
-    <option value="-11">-11</option>
-    `
-}
-
-timezoneOptions();
 clock();
